86dec8f86108de5c439fe2025c5635b797a66adb
Waldo Jaquith <waldo@jaquith.org>
2012-05-14 21:19:00 -0400

    Filename variables, CSV writer, DB creation SQL, and instructions.
    Added variables to control the names of the files that are created,
    created an incomplete instance of csv.writer to save the cleaned-up
    CSV, added the CREATE TABLE statement and the functionality to save it
    to a file, and added text that's displayed at the conclusion of the
    process to provide instructions.

--- parser.py
+++ parser.py
@@ -1,6 +1,12 @@
 # Import needed libraries.
 import csv, urllib2, re, time
 
+# The name of the file to which we're saving the cleaned-up CSV.
+output_filename = 'import.csv'
+
+# The name of the file to which we're saving the SQL to import this data.
+sql_filename = 'import.sql'
+
 # This will only work for the month of April. Because there's no single URL for the report, it's
 # going to be necessary to either iterate through all directories or get the most recent one. Until
 # the SBE documents or explains what they're doing here, it's tough to know what do with this.
@@ -13,6 +19,10 @@ csv_file = urllib2.urlopen(url)
 # Convert the contents of the CSV file to a list.
 # If this fails, throw an error and quit.
 Report = csv.reader(csv_file)
+
+# Convert list back to a CSV file and write to the filesystem.
+output_file = open(output_filename, 'wb')
+wr = csv.writer(output_file)
 
 # Iterate through each line in the report.
 for counter, row in enumerate(Report):
@@ -42,53 +52,72 @@ for counter, row in enumerate(Report):
 		if len(row['ElectionCycle']) > 0:
 			row['ElectionCycle'] = time.strftime('%Y-%m-%d', time.strptime(row['ElectionCycle'], '%m/%Y'))
 		
-		print (row)
-
-
-# Convert list back to a CSV file and write to the filesystem.
+		# Commented out because it's not working.
+		#wr.writerow(row)
 
 # Create a SQL file that will create the table(s) and load the CSV from file.
+sql_file = open(sql_filename, 'wb')
+sql_create = """CREATE TABLE IF NOT EXISTS `filings` (
+  `ReportId` mediumint(8) unsigned NOT NULL,
+  `AccountId` char(38) collate utf8_bin NOT NULL,
+  `CommitteeCode` varchar(12) collate utf8_bin NOT NULL,
+  `CommitteeName` varchar(128) collate utf8_bin NOT NULL,
+  `CommitteeType` varchar(128) collate utf8_bin NOT NULL,
+  `CandidateName` varchar(128) collate utf8_bin default NULL,
+  `IsStateWide` enum('y','n') collate utf8_bin NOT NULL default 'n',
+  `IsGeneralAssembly` enum('y','n') collate utf8_bin NOT NULL,
+  `IsLocal` enum('y','n') collate utf8_bin NOT NULL,
+  `Party` varchar(64) collate utf8_bin NOT NULL,
+  `FecNumber` char(9) collate utf8_bin default NULL,
+  `ReportYear` year(4) NOT NULL,
+  `FilingDate` datetime NOT NULL,
+  `StartDate` datetime NOT NULL,
+  `EndDate` datetime NOT NULL,
+  `AddressLine1` varchar(128) collate utf8_bin NOT NULL,
+  `AddressLine2` varchar(128) collate utf8_bin default NULL,
+  `AddressLine3` varchar(128) collate utf8_bin default NULL,
+  `City` varchar(128) collate utf8_bin NOT NULL,
+  `StateCode` char(2) collate utf8_bin NOT NULL default 'VA',
+  `ZipCode` varchar(10) collate utf8_bin NOT NULL,
+  `FilingType` enum('report') collate utf8_bin NOT NULL default 'report',
+  `IsFinalReport` enum('y','n') collate utf8_bin NOT NULL default 'n',
+  `IsAmendment` enum('y','n') collate utf8_bin NOT NULL default 'n',
+  `AmendmentCount` tinyint(3) unsigned NOT NULL,
+  `SubmitterPhone` varchar(12) collate utf8_bin NOT NULL,
+  `SubmitterEmail` varchar(128) collate utf8_bin NOT NULL,
+  `ElectionCycle` date NOT NULL,
+  `ElectionCycleStartDate` datetime NOT NULL,
+  `ElectionCycleEndDate` datetime NOT NULL,
+  `OfficeSought` varchar(128) collate utf8_bin NOT NULL,
+  `District` varchar(128) collate utf8_bin NOT NULL,
+  `NoActivity` enum('y','n') collate utf8_bin NOT NULL default 'n',
+  `BalanceLastReportingPeriod` decimal(10,2) NOT NULL,
+  `DateOfReferendum` datetime NOT NULL,
+  `SubmittedDate` datetime NOT NULL,
+  `DueDate` datetime NOT NULL,
+  PRIMARY KEY  (`ReportId`),
+  KEY `AccountId` (`AccountId`,`CandidateName`),
+  KEY `Party` (`Party`,`FecNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;"""
+sql_create += '\n\nLOAD DATA LOCAL INFILE "' + output_filename
+sql_create += """" \n\
+INTO TABLE filings \n\
+FIELDS TERMINATED BY "," \n\
+ENCLOSED BY '"' \n\
+LINES TERMINATED BY '\\n' \n\
+(ReportId, AccountId, CommitteeCode, CommitteeName, CommitteeType, CandidateName, IsStateWide, \
+IsGeneralAssembly, IsLocal, Party, FecNumber, ReportYear, FilingDate, StartDate, EndDate, \
+AddressLine1, AddressLine2, AddressLine3, City, StateCode, ZipCode, FilingType, IsFinalReport, \
+IsAmendment, AmendmentCount, SubmitterPhone, SubmitterEmail, ElectionCycle, ElectionCycleStartDate, \
+ElectionCycleEndDate, OfficeSought, District, NoActivity, BalanceLastReportingPeriod, \
+DateofReferendum, SubmittedDate, DueDate)"""
+sql_file.write(sql_create)
+sql_file.close
 
 # Echo instructions for how to load the SQL into MySQL.
-	
-"""
-Mapping of field values to SQL column.
-
-ReportId					mediumint unsigned
-AccountId					38
-CommitteeCode				varchar (12)
-CommitteeName				varchar (not null)
-CommitteeType				enum (create list based on distinct values)
-CandidateName				varchar (can be null)
-IsStateWide					true/false
-IsGeneralAssembly			true/false
-IsLocal						true/false
-Party						enum (create list based on distinct values)
-FecNumber					????
-ReportYear					year
-FilingDate					datetime (microtime)
-StartDate					datetime
-EndDate						datetime
-AddressLine1				varchar
-AddressLine2				varchar
-AddressLine3				varchar
-City						varchar (normalize case)
-StateCode					char (2)
-ZipCode						varchar (5-10 characters)
-FilingType					enum (Report, ???)
-IsFinalReport				true/false
-IsAmendment					true/false
-AmendmentCount				tinyint unsigned
-SubmitterPhone				varchar (normalize format)
-SubmitterEmail				varchar
-ElectionCycle				date (convert from MM/YYYY to MM/DD/YYYY)
-ElectionCycleStartDate		datetime
-ElectionCycleEndDate		datetime
-OfficeSought				varchar (strip out prefix)
-District					varchar (strip out prefix)
-NoActivity					true/false
-BalanceLastReportingPeriod	money (dollars cents)
-DateOfReferendum			datetime
-SubmittedDate				datetime
-DueDate						datetime
-"""
+print "CSV and SQL exported successfully. To load the database into MySQL, run the \n\
+following command:\n\
+\n\
+mysql [database] < " + sql_filename + "\n\
+\n\
+Replace [database] with the name of the database in which you want to store\nthe data.";
